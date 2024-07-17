3a1a4a5ae8ade0f1951dcc11e732c1f712ae87f7
DrBenSullivan <ben.sullivan1992@gmail.com>
2024-07-17 12:48:02 +0100

    Added stock trade view model as per requirement.

--- Controllers/TradeController.cs
+++ Controllers/TradeController.cs
@@ -2,7 +2,7 @@
 
 namespace StocksAppWithConfiguration.Controllers
 {
-	public class HomeController : Controller
+	public class TradeController : Controller
 	{
 		[Route("/")]
 		public IActionResult Index()
---
+++ Models/ViewModels/StockTradeViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace StocksAppWithConfiguration.Models.ViewModels
+{
+    public class StockTradeViewModel
+    {
+        string? StockSymbol { get; set; }
+        string? StockName { get; set; }
+        double Price { get; set; }
+        int Quantity { get; set; }
+    }
+}
