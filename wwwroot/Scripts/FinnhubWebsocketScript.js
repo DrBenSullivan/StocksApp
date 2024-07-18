80e855ced9aa928e8f50bff97fb91407f2954a3c
DrBenSullivan <ben.sullivan1992@gmail.com>
2024-07-18 16:57:34 +0100

    JS websocket script correctly requesting data. Need to update view with new prices.

--- wwwroot/Scripts/FinnhubWebsocketScript.js
+++ wwwroot/Scripts/FinnhubWebsocketScript.js
@@ -1,1 +1,18 @@
-﻿document.querySelector("#testSpan").innerHTML = "YES";
+﻿$(document).ready(function () {
+    const finnhubKey = $('#finnhub-api-key').val();
+    const stockSymbol = $('#stock-symbol').html();
+
+    const socket = new WebSocket(`wss://ws.finnhub.io?token=${finnhubKey}`);
+    socket.addEventListener('open', function (event) {
+        socket.send(JSON.stringify({ 'type': 'subscribe', 'symbol': `${stockSymbol}` }))
+    });
+    socket.addEventListener('message', function (event) {
+        const jsonTrades = JSON.parse(event.data);
+        console.log(jsonTrades);
+    });
+
+    window.addEventListener('beforeunload', function () {
+        socket.send(JSON.stringify({ 'type': 'unsubscribe', 'symbol': `${stockSymbol}` }));
+        socket.close();
+    });
+});
