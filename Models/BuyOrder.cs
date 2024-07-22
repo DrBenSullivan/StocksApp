0a59af5c5d4bef0d7a7bfbc76ef35308b42b37fd
DrBenSullivan <ben.sullivan1992@gmail.com>
2024-07-22 09:28:52 +0100

    Redeveloping for next assignment

---
+++ Models/BuyOrder.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StocksAppWithConfiguration.Models
+{
+	public class BuyOrder
+	{
+		Guid? BuyOrderID { get; set; } = Guid.NewGuid();
+
+		[Required]
+		string StockSymbol { get; set; }
+
+		[Required]
+		string StockName { get; set; }
+
+		DateTime? DateAndTimeOfOrder { get; set; } = DateTime.Now;
+
+		[Range(1, 100000)]
+		int? Quantity { get; set; }
+
+		[Range(1, 10000)]
+		double? Price { get; set; }
+	}
+}
