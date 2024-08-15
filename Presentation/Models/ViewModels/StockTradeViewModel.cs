dc1e49aa719a7a324102e0b067b80a7607b07b56
DrBenSullivan <ben.sullivan1992@gmail.com>
2024-08-15 12:00:18 +0100

    Initial commit.

--- Presentation/Models/ViewModels/StockTradeViewModel.cs
+++ Presentation/Models/ViewModels/StockTradeViewModel.cs
@@ -1,10 +1,13 @@
-﻿namespace StocksApp.Presentation.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StocksApp.Presentation.Models.ViewModels
 {
     public class StockTradeViewModel
     {
         public string? StockSymbol { get; set; }
         public string? StockName { get; set; }
         public double Price { get; set; }
+		[Range(0, 100000, ErrorMessage = "A valid stock Quantity between 1 - 100,000 was not provided.")]
         public int Quantity { get; set; }
     }
 }
