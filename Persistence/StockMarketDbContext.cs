16fe2e7be7954b57070245d47fb54bd987b6c9f1
DrBenSullivan <ben.sullivan1992@gmail.com>
2024-08-20 12:09:45 +0100

    Add StockMarketDbContext

---
+++ Persistence/StockMarketDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StocksApp.Domain.Models;
+
+namespace StocksApp.Persistence
+{
+	public class StockMarketDbContext : DbContext
+	{
+		public DbSet<BuyOrder> BuyOrders { get; set; }
+		public DbSet<SellOrder> SellOrders { get; set; }
+	}
+}
