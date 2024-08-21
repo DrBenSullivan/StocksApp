2664f035570e4ebdae0a9cfe42190066df5566c8
DrBenSullivan <ben.sullivan1992@gmail.com>
2024-08-21 09:35:56 +0100

    Create OrdersPdfViewModel

--- Presentation/Models/ViewModels/OrdersPdfViewModel.cs
+++ Presentation/Models/ViewModels/OrdersPdfViewModel.cs
@@ -5,9 +5,20 @@ namespace StocksApp.Presentation.Models.ViewModels
 {
 	public class OrdersPdfViewModel
 	{
-		public List<Order> Orders { get; set; } = new List<Order>();
+		public List<OrderResponse> Orders { get; set; } = new List<OrderResponse>();
 
-		public OrdersPdfViewModel(List<>
+		public OrdersPdfViewModel(List<BuyOrderResponse>? buyOrders, List<SellOrderResponse>? sellOrders)
+		{
+			if (buyOrders != null)
+				Orders.AddRange(buyOrders);
+
+			if (sellOrders != null)
+				Orders.AddRange(sellOrders);
+
+			Orders = Orders
+				.OrderBy(o => o.DateAndTimeOfOrder)
+				.ToList();
+		}
 	}
 }
 
