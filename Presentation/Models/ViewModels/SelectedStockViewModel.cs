0ce1986c2c2da5f542cc2125fdb3322b57a6b6a3
DrBenSullivan <ben.sullivan1992@gmail.com>
2024-09-19 10:08:34 +0100

    Add IFinnhubRepository

---
+++ Presentation/Models/ViewModels/SelectedStockViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace StocksApp.Presentation.Models.ViewModels
+{
+	public class SelectedStockViewModel
+	{
+		public string? StockName { get; set; }
+		public string? StockSymbol { get; set; }
+		public string? LogoUrl { get; set; }
+		public string? Sector { get; set; }
+		public string? Exchange { get; set; }
+		public double? Price { get; set; }
+	}
+}
--- ViewComponents/SelectedStockViewComponent.cs
+++ ViewComponents/SelectedStockViewComponent.cs
@@ -1,9 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Win32;
+using StocksApp.Application.Interfaces;
+using StocksApp.Presentation.Models.ViewModels;
 
 namespace StocksApp.ViewComponents
 {
+    [ViewComponent]
 	public class SelectedStockViewComponent : ViewComponent
 	{
+		#region private readonly fields
+		private readonly IFinnhubService _finnhubService;
+		#endregion
 
+		#region constructor
+		public SelectedStockViewComponent(IFinnhubService finnhubService)
+        {
+            _finnhubService = finnhubService;
+        }
+		#endregion
+
+   //     public async Task<IViewComponentResult> InvokeAsync(string? stockSymbol)
+   //     {
+			//if (stockSymbol.IsNullOrEmpty()) return View(null);
+
+			//Dictionary<string, object>? companyProfile = await _finnhubService.GetCompanyProfile(stockSymbol)
+			//	?? throw new Exception("No response from API from given stock symbol.");
+
+			//var viewModel = new SelectedStockViewModel()
+			//{
+			//	StockName = companyProfile.name ?? string.Empty,
+
+			//};
+
+   //     }
 	}
 }
