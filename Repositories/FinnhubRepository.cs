f15c135c6eb8be3589df483d44e1b4db8facfb67
DrBenSullivan <ben.sullivan1992@gmail.com>
2024-09-19 10:09:45 +0100

    Add FinnhubRepository

---
+++ Repositories/FinnhubRepository.cs
@@ -0,0 +1,27 @@
+﻿using StocksApp.Repositories.Interfaces;
+
+namespace StocksApp.Repositories
+{
+	public class FinnhubRepository : IFinnhubRepository
+	{
+		public Task<Dictionary<string, object>?> GetCompanyProfile(string stockSymbol)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<Dictionary<string, string>>?> GetStocks()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Dictionary<string, object>?> SearchStocks(string stockSymbolToSearch)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
