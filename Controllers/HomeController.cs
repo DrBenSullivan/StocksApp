0b6c9fa65a99dc107e7bbc3d1bf0f344c0e22e7b
DrBenSullivan <ben.sullivan1992@gmail.com>
2024-09-23 10:07:15 +0100

    Final version

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StocksApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		[Route("/Error")]
+		public IActionResult Error()
+		{
+			IExceptionHandlerFeature? feature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+			if (feature != null && feature.Error != null)
+			{
+				ViewBag.ErrorMessage = feature.Error.Message;
+			}
+			return View();
+		}
+	}
+}
--- Filters/BuyOrderAndSellOrderActionFilter.cs
+++ Filters/BuyOrderAndSellOrderActionFilter.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace StocksApp.Filters
 {
-    public class BuyOrderAndSellOrderActionFilter : IActionFilter
+    public class BuyOrderAndSellOrderActionFilter : ActionFilterAttribute
     {
 
         public override void OnActionExecuting(ActionExecutingContext context)
